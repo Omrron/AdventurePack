119fdd5d5e4794c6216bfc6c7e0b1cfcf97f5fda
Nadav Aharon <azxza23@hotmail.com>
2025-03-22 02:34:51 +0200

    updated quests

--- kubejs/server_scripts/generalRecipes.js
+++ kubejs/server_scripts/generalRecipes.js
@@ -1,3 +1,5 @@
 ServerEvents.recipes(event => {
     event.shapeless(Item.of('minecraft:black_dye', 1), ['minecraft:ink_sac']);
-})
+    const recipes = event.getRecipes();
+    console.log(recipes);
+});
