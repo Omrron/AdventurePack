4b8a1eea833c1263023b23a69572dc30ef5d48e6
Omrron <obaron4120@gmail.com>
2025-03-28 19:34:18 +0300

    Many a config

---
+++ kubejs/server_scripts/recipesBlockers.js
@@ -0,0 +1,25 @@
+ServerEvents.recipes((event) => {
+
+    event.addedRecipes(event.recipes())
+
+    /**
+     * @param {Internal.RecipeFilter_[]}
+     * @param {Internal.Ingredient_} ouput
+     */
+    const filters = [{mod:"irons_spellbooks"}, {type:"irons_spellbooks:scroll_forge"}, {output:"oak_planks"}];
+    event.printAllTypes();
+    removeRecipeFromStage(filters, "one", event);
+});
+
+/**
+ * @param {Internal.RecipeFilter_} filters 
+ * @param {string} stage 
+ * @param {Internal.RecipesEventJS} event 
+ */
+const removeRecipeFromStage = (filters, stage, event) => {
+    filters.forEach((filter) => {
+        event.forEachRecipe(filter, (b) => {
+            event.addRecipe(b, true).stage(stage);
+        })
+    });
+}
