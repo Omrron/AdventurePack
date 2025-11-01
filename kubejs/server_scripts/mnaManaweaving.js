29dbe552c96e3916960dbafb3029500bd285eb80
Omrron <obaron4120@gmail.com>
2025-11-01 17:45:47 +0200

    Updated quests and recipes
    -Removed private files from test world
    - Added recipe for mutavis
    - Changed quests to match

--- kubejs/server_scripts/mnaManaweaving.js
+++ kubejs/server_scripts/mnaManaweaving.js
@@ -1,40 +1,29 @@
 ServerEvents.recipes(event => {
+
     event.custom({
         type: "mna:manaweaving-recipe",
         tier: 1,
-        output: "hexalia:sunfire_tomato_seeds",
+        output: "hexalia:mutavis",
         quantity: 1,
-        items: ["minecraft:wheat_seeds"],
-        patterns: ["mna:manaweave_patterns/circle"],
-    });
-    event.custom({
-        type: "mna:manaweaving-recipe",
-        tier: 1,
-        output: "hexalia:siren_kelp",
-        quantity: 1,
-        items: ["minecraft:kelp"],
-        patterns: ["mna:manaweave_patterns/triangle"],
-    });
-    event.custom({
-        type: "mna:manaweaving-recipe",
-        tier: 1,
-        output: "hexalia:mandrake_seeds",
-        quantity: 1,
-        items: ["minecraft:wheat_seeds"],
-        patterns: ["mna:manaweave_patterns/square"],
+        items: ["minecraft:potato", "hexalia:tree_resin", "minecraft:kelp", "minecraft:poppy", "minecraft:wheat_seeds", "minecraft:bone_meal"],
+        patterns: [
+            "mna:manaweave_patterns/circle",
+            "mna:manaweave_patterns/square",
+            "mna:manaweave_patterns/triangle"
+        ],
     });
     
     event.custom(/**
         * @type {Internal.RecipeJS}
         */{
-        type: 'mna:manaweaving-recipe',
+        type: "mna:manaweaving-recipe",
         tier: 3,
         output: Item.of("irons_spellbooks:common_ink"),
         items: [
-            'minecraft:ink_sac',
-            'irons_spellbooks:arcane_essence',
-            'mna:chimerite_gem',
-            'minecraft:glass_bottle',
+            "minecraft:ink_sac",
+            "irons_spellbooks:arcane_essence",
+            "mna:chimerite_gem",
+            "minecraft:glass_bottle",
         ],
         patterns: [
             "mna:manaweave_patterns/circle",
@@ -46,15 +35,15 @@ ServerEvents.recipes(event => {
     event.custom(/**
         * @type {Internal.RecipeJS}
         */{
-        type: 'mna:manaweaving-recipe',
+        type: "mna:manaweaving-recipe",
         tier: 3,
         output: Item.of("irons_spellbooks:common_ink"),
         quantity: 8,
         items: [
-            'minecraft:ink_sac',
-            'irons_spellbooks:arcane_essence',
-            'minecraft:nether_star',
-            'minecraft:glass_bottle',
+            "minecraft:ink_sac",
+            "irons_spellbooks:arcane_essence",
+            "minecraft:nether_star",
+            "minecraft:glass_bottle",
         ],
         patterns: [
             "mna:manaweave_patterns/circle",
@@ -66,14 +55,14 @@ ServerEvents.recipes(event => {
     event.custom(/**
         * @type {Internal.RecipeJS}
         */{
-            type: 'mna:manaweaving-recipe',
+            type: "mna:manaweaving-recipe",
             tier: 1,
             output: Item.of("irons_spellbooks:iron_spell_book"),
             quantity: 1,
             items: [
-                'minecraft:book',
-                'minecraft:iron_ingot',
-                'mna:vinteum_dust'
+                "minecraft:book",
+                "minecraft:iron_ingot",
+                "mna:vinteum_dust"
             ],
             patterns: [
                 "mna:manaweave_patterns/circle",
