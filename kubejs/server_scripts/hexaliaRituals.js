db97aa35b72e5d6c90e330ccf547a2d515c9d162
Omri <obaron4120@gmail.com>
2025-11-04 00:52:36 +0200

    Learned kubejs hexalia rituals

--- kubejs/server_scripts/hexaliaRituals.js
+++ kubejs/server_scripts/hexaliaRituals.js
@@ -1,3 +1,26 @@
-// ServerEvents.recipes((event) => {
-//   event.custom({});
-// });
+ServerEvents.recipes((event) => {
+
+    /*TODO: 
+    - Add recipes for other nodes
+    - Remove old recipes
+    - Checkout mutavis convertion recipes
+    - Maybe checkout cauldron recipes 
+    */
+    event.custom({
+        "type": "hexalia:ritual_table",
+        "ingredients": [
+            {
+            "item": "minecraft:amethyst_shard"
+            },
+            {
+            "item": "hexalia:sunfire_tomato"
+            },
+            {
+            "item": "minecraft:dandelion"
+            }
+        ],
+        "output": {
+            "item": "hexalia:fire_node"
+        }
+    });
+});
