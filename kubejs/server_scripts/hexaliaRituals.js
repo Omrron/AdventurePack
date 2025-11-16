576fbee8026e4c8e13d15e48bf50db701a6bc04a
Omri <obaron4120@gmail.com>
2025-11-17 01:32:00 +0200

    Fixed hexalia book
    added ids to removed recipes
    also added description how to make a ritual table

--- kubejs/server_scripts/hexaliaRituals.js
+++ kubejs/server_scripts/hexaliaRituals.js
@@ -5,75 +5,83 @@ ServerEvents.recipes((event) => {
     - Checkout mutavis convertion recipes
     - Maybe checkout cauldron recipes 
     */
-  event.custom({
-    type: "hexalia:ritual_table",
-    ingredients: [
-      {
-        item: "minecraft:amethyst_shard",
+  event
+    .custom({
+      type: "hexalia:ritual_table",
+      ingredients: [
+        {
+          item: "minecraft:amethyst_shard",
+        },
+        {
+          item: "hexalia:sunfire_tomato",
+        },
+        {
+          item: "minecraft:sunflower",
+        },
+      ],
+      output: {
+        item: "hexalia:fire_node",
       },
-      {
-        item: "hexalia:sunfire_tomato",
-      },
-      {
-        item: "minecraft:sunflower",
-      },
-    ],
-    output: {
-      item: "hexalia:fire_node",
-    },
-  });
+    })
+    .id("hexalia:fire_node_from_ritual_table");
 
-  event.custom({
-    type: "hexalia:ritual_table",
-    ingredients: [
-      {
-        item: "minecraft:amethyst_shard",
+  event
+    .custom({
+      type: "hexalia:ritual_table",
+      ingredients: [
+        {
+          item: "minecraft:amethyst_shard",
+        },
+        {
+          item: "hexalia:galeberries",
+        },
+        {
+          item: "minecraft:feather",
+        },
+      ],
+      output: {
+        item: "hexalia:air_node",
       },
-      {
-        item: "hexalia:galeberries",
-      },
-      {
-        item: "minecraft:feather",
-      },
-    ],
-    output: {
-      item: "hexalia:air_node",
-    },
-  });
+    })
+    .id("hexalia:air_node_from_ritual_table");
 
-  event.custom({
-    type: "hexalia:ritual_table",
-    ingredients: [
-      {
-        item: "minecraft:amethyst_shard",
+  event
+    .custom({
+      type: "hexalia:ritual_table",
+      ingredients: [
+        {
+          item: "minecraft:amethyst_shard",
+        },
+        {
+          item: "hexalia:siren_paste",
+        },
+        {
+          item: "minecraft:ink_sac",
+        },
+      ],
+      output: {
+        item: "hexalia:water_node",
       },
-      {
-        item: "hexalia:siren_paste",
-      },
-      {
-        item: "minecraft:ink_sac",
-      },
-    ],
-    output: {
-      item: "hexalia:water_node",
-    },
-  });
+    })
+    .id("hexalia:water_node_from_ritual_table");
 
-  event.custom({
-    type: "hexalia:ritual_table",
-    ingredients: [
-      {
-        item: "minecraft:amethyst_shard",
+  event
+    .custom({
+      type: "hexalia:ritual_table",
+      ingredients: [
+        {
+          item: "minecraft:amethyst_shard",
+        },
+        {
+          item: "hexalia:mandrake",
+        },
+        {
+          item: "minecraft:clay_ball",
+        },
+      ],
+      output: {
+        item: "hexalia:earth_node",
       },
-      {
-        item: "hexalia:mandrake",
-      },
-      {
-        item: "minecraft:clay_ball",
-      },
-    ],
-    output: {
-      item: "hexalia:earth_node",
-    },
-  });
+    })
+    .id("hexalia:earth_node_from_ritual_table");
 });
