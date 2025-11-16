f383a0589e3756ddfb0a03932f72d37d9c4da1dd
Omri <obaron4120@gmail.com>
2025-11-16 23:04:04 +0200

    Renamed another rarity

--- kubejs/client_scripts/renaming_en_us.js
+++ kubejs/client_scripts/renaming_en_us.js
@@ -1,5 +1,6 @@
 ClientEvents.lang("en_us", (event) => {
   // 'en_us' is the name of the U.S. English asset file (assets/mod_id/lang/en_us.json)
   event.add("mna:rituals/summon_mutant_skeleton", "Summon Mutant Skeleton");
-  event.add("alexscaves.rainbow", "Ascended");
+  event.add("alexscaves.rainbow", "Radiant");
+  event.add("alexscaves.demonic", "Demonic");
 });
